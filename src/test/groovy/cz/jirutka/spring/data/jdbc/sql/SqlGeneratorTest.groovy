fa6220eb08eacdde05c7d4d5f93d599faf6cf698
Jakub Jirutka <jakub@jirutka.cz>
2016-03-13 22:36:37 +0100

    Refactor and extend tests for SqlGenerators

--- src/test/groovy/cz/jirutka/spring/data/jdbc/sql/SqlGeneratorTest.groovy
+++ src/test/groovy/cz/jirutka/spring/data/jdbc/sql/SqlGeneratorTest.groovy
@@ -17,96 +17,199 @@
 package cz.jirutka.spring.data.jdbc.sql
 
 import cz.jirutka.spring.data.jdbc.TableDescription
+import org.springframework.data.domain.PageRequest
+import org.springframework.data.domain.Pageable
+import org.springframework.data.domain.Sort
+import org.springframework.data.domain.Sort.Direction
+import org.springframework.data.domain.Sort.Order
 import spock.lang.Specification
 import spock.lang.Unroll
+
+import static org.springframework.data.domain.Sort.Direction.ASC
+import static org.springframework.data.domain.Sort.Direction.DESC
 
 @Unroll
 class SqlGeneratorTest extends Specification {
 
     final ANY = new Object()
-    static final idClause = 'num1 = ? AND num2 = ? AND num3 = ?'
 
-    def sqlGenerator = new SqlGenerator()
+    def table = new TableDescription (
+        tableName: 'tab',
+        selectClause: 'a, b',
+        fromClause: 'tabx',
+        pkColumns: ['tid']
+    )
+
+    def getSqlGenerator() { new SqlGenerator() }
 
 
-    def 'selectByIds(): when single id column and given #desc'() {
-        given:
-            def table = new TableDescription('table', 'num')
+    def 'count()'() {
+        expect:
+            sqlGenerator.count(table) == 'SELECT count(*) FROM tabx'
+    }
+
+
+    def 'deleteAll()'() {
+        expect:
+            sqlGenerator.deleteAll(table) == 'DELETE FROM tab'
+    }
+
+
+    def 'deleteById(): with #desc'() {
+        setup:
+            table.pkColumns = pkColumns(pkSize)
+        expect:
+            sqlGenerator.deleteById(table) == "DELETE FROM tab WHERE ${pkPredicate(pkSize)}"
+        where:
+            pkSize || desc
+            1      || 'simple PK'
+            3      || 'composite PK'
+    }
+
+
+    def 'existsById(): with #desc'() {
+        setup:
+          table.pkColumns = pkColumns(pkSize)
+        expect:
+            sqlGenerator.existsById(table) == "SELECT 1 FROM tab WHERE ${pkPredicate(pkSize)}"
+        where:
+            pkSize || desc
+            1      || 'simple PK'
+            3      || 'composite PK'
+    }
+
+
+    def 'insert()'() {
         when:
-            def actual = sqlGenerator.selectByIds(table, idsCount)
+            def actual = sqlGenerator.insert(table, [x: ANY, y: ANY, z: ANY])
         then:
-            actual == "SELECT * FROM table${expected}"
+            actual == 'INSERT INTO tab (x, y, z) VALUES (?, ?, ?)'
+    }
+
+
+    def 'selectAll()'() {
+        expect:
+            sqlGenerator.selectAll(table) == 'SELECT a, b FROM tabx'
+    }
+
+    def "selectAll(Pageable): #desc"() {
+        setup:
+            table.pkColumns = pkColumns(pkSize)
+            def expected = expectedPaginatedQuery(table, pageable)
+        expect:
+            sqlGenerator.selectAll(table, pageable) == expected
+        where:
+            pkSize | pageable                      || desc
+            1      | page(0, 10)                   || 'when simple key and requested first page'
+            1      | page(20, 10)                  || 'when simple key and requested third page'
+            1      | page(0, 10, order(ASC, 'a'))  || 'when simple key and requested first page with sort'
+            3      | page(0, 10)                   || 'when composite key and requested first page'
+            3      | page(20, 10, order(ASC, 'a')) || 'when composite key and requested third page with sort'
+    }
+
+    def 'selectAll(Sort): #expected'() {
+        when:
+            def actual = sqlGenerator.selectAll(table, new Sort(orders))
+        then:
+            actual == "SELECT a, b FROM tabx ${expected}"
+        where:
+            orders                              || expected
+            [order(ASC, 'a')]                   || 'ORDER BY a ASC'
+            [order(DESC, 'a')]                  || 'ORDER BY a DESC'
+            [order(ASC, 'a'), order(DESC, 'b')] || 'ORDER BY a ASC, b DESC'
+    }
+
+
+    def 'selectById(): with #desc'() {
+        setup:
+            table.pkColumns = pkColumns(pkSize)
+        expect:
+            sqlGenerator.selectById(table) == "SELECT a, b FROM tabx WHERE ${pkPredicate(pkSize)}"
+        where:
+            pkSize || desc
+            1      || 'simple PK'
+            3      || 'composite PK'
+    }
+
+
+    def 'selectByIds(): when simple PK and given #desc'() {
+        expect:
+            sqlGenerator.selectByIds(table, idsCount) == "SELECT a, b FROM tabx${expected}"
         where:
             idsCount || expected                  | desc
             0        || ''                        | 'no id'
-            1        || ' WHERE num = ?'          | 'one id'
-            2        || ' WHERE num IN (?, ?)'    | 'two ids'
-            3        || ' WHERE num IN (?, ?, ?)' | 'several ids'
+            1        || ' WHERE tid = ?'          | 'one id'
+            2        || ' WHERE tid IN (?, ?)'    | 'two ids'
+            3        || ' WHERE tid IN (?, ?, ?)' | 'several ids'
     }
 
-    def 'selectByIds(): when multiple id columns and given #desc'() {
-        given:
-            def table = new TableDescription('table', null, 'num1', 'num2', 'num3')
+    def 'selectByIds(): when composite PK and given #desc'() {
+        setup:
+            table.pkColumns = pkColumns(3)
+            expected = expected.replaceAll('%1', pkPredicate(3))
         when:
             def actual = sqlGenerator.selectByIds(table, idsCount)
         then:
-            actual == "SELECT * FROM table${expected}"
+            actual == "SELECT a, b FROM tabx${expected}"
         where:
-            idsCount || expected                                                 | desc
-            0        || ''                                                       | 'no id'
-            1        || " WHERE ${idClause}"                                     | 'one id'
-            2        || " WHERE (${idClause}) OR (${idClause})"                  | 'two ids'
-            3        || " WHERE (${idClause}) OR (${idClause}) OR (${idClause})" | 'several ids'
+            idsCount || expected                      | desc
+            0        || ''                            | 'no id'
+            1        || ' WHERE %1'                   | 'one id'
+            2        || ' WHERE (%1) OR (%1)'         | 'two ids'
+            3        || ' WHERE (%1) OR (%1) OR (%1)' | 'several ids'
     }
 
-    def 'deleteById(): with #desc'() {
-        given:
-            def idColumns = idColumns(idsCount)
-            def table = new TableDescription('table', null, *idColumns)
-        when:
-            def actual = sqlGenerator.deleteById(table)
-        then:
-            actual == "DELETE FROM table WHERE ${whereIds(idsCount)}"
-        where:
-            idsCount || desc
-            1        || 'single id column'
-            2        || 'two id columns'
-            3        || 'several id columns'
-    }
 
     def 'update(): with #desc'() {
-        given:
-            def idColumns = idColumns(idsCount)
-            def table = new TableDescription('table', null, *idColumns)
+        setup:
+            table.pkColumns = pkColumns(idsCount)
         when:
             def actual = sqlGenerator.update(table, [x: ANY, y: ANY, z: ANY])
         then:
-            actual == "UPDATE table SET x = ?, y = ?, z = ? WHERE ${whereIds(idsCount)}"
+            actual == "UPDATE tab SET x = ?, y = ?, z = ? WHERE ${pkPredicate(idsCount)}"
         where:
             idsCount || desc
-            1        || 'single id column'
-            2        || 'multiple id columns'
-    }
-
-    def 'existsById(): with #desc'() {
-        given:
-            def table = new TableDescription('table', null, *idColumns(idsCount))
-        when:
-            def actual = sqlGenerator.existsById(table)
-        then:
-            actual == "SELECT 1 FROM table WHERE ${whereIds(idsCount)}"
-        where:
-            idsCount || desc
-            1        || 'single id column'
-            2        || 'multiple id columns'
+            1        || 'simple PK'
+            2        || 'composite PK'
     }
 
 
-    private idColumns(count) {
-        [0..count].collect { "num${it}" }
+    def expectedPaginatedQuery(TableDescription table, Pageable page) {
+
+        // If sort is not specified, then it should be sorted by primary key columns.
+        def sort = page.sort ?: new Sort(ASC, table.pkColumns)
+
+        def firstIndex = page.offset + 1
+        def lastIndex = page.offset + page.pageSize
+
+        """
+            SELECT t2__.* FROM (
+                SELECT row_number() OVER (${orderBy(sort)}) AS rn__, t1__.* FROM (
+                    SELECT ${table.selectClause} FROM ${table.fromClause}
+                ) t1__
+            ) t2__ WHERE t2__.rn__ BETWEEN ${firstIndex} AND ${lastIndex}
+        """.trim().replaceAll(/\s+/, ' ')
     }
 
-    private whereIds(count) {
-        idColumns(count).collect { "$it = ?" }.join(' AND ')
+
+    def page(int offset, int limit, Order... orders) {
+        def sort = orders.length > 0 ? new Sort(orders) : null
+        new PageRequest(offset / limit as int, limit, sort)
+    }
+
+    def order(Direction dir, String property) {
+        new Order(dir, property)
+    }
+
+    def orderBy(Sort sort) {
+        'ORDER BY ' + sort.collect { "${it.property} ${it.direction.name()}" }.join(', ')
+    }
+
+    def pkColumns(count) {
+        (1..count).collect { "id${it}" }*.toString()
+    }
+
+    def pkPredicate(count) {
+        pkColumns(count).collect { "$it = ?" }.join(' AND ')
     }
 }
